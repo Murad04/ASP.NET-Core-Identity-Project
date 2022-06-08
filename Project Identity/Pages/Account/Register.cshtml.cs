7c746d64f626bd7fc0cd0df89651bb0b399cd9b9
Murad <67361462+Murad04@users.noreply.github.com>
2022-06-08 20:48:24 +0400

    User registration update

---
+++ Project Identity/Pages/Account/Register.cshtml.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Project_Identity.ViewModels;
+
+namespace Project_Identity.Pages.Account
+{
+    public class RegisterModel : PageModel
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        public RegisterModel(UserManager<IdentityUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        [BindProperty]
+        public RegisterViewModel registerViewModel { get; set; } = null!;
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid) return Page();
+
+            //Validating email address
+
+            //Create the user
+            var user = new IdentityUser
+            {
+                Email = registerViewModel.Email,
+                UserName = registerViewModel.Email
+            };
+
+            var result = await this.userManager.CreateAsync(user, registerViewModel.Password);
+            if (result.Succeeded)
+            {
+                return RedirectToPage("/Account/Login");
+            }
+            else
+            {
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError("Register", error.Description);
+                }
+                return Page();
+            }
+        }
+    }
+}
---
+++ Project Identity/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Project_Identity.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress(ErrorMessage = "Invalid email")]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        [DataType(dataType: DataType.Password)]
+        public string Password { get; set; } = null!;
+    }
+}
