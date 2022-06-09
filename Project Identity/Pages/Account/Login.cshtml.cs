14cab67ae4f5dbbfcd012b84734f9639fd048f0a
Murad <67361462+Murad04@users.noreply.github.com>
2022-06-09 18:50:32 +0400

    User login with Identity

---
+++ Project Identity/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Project_Identity.ViewModels;
+
+namespace Project_Identity.Pages.Account
+{
+    public class LoginModel : PageModel
+    {
+        private readonly SignInManager<IdentityUser> signInManager;
+
+        public LoginModel(SignInManager<IdentityUser> signInManager)
+        {
+            this.signInManager = signInManager;
+        }
+
+        [BindProperty]
+        public CredentialViewModel Credential { get; set; } = null!;
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid) return Page();
+
+            var result = await signInManager.PasswordSignInAsync(this.Credential.Email, this.Credential.Password, this.Credential.RememberMe, false);
+
+            if (result.Succeeded)
+                return RedirectToPage("/Index");
+            else
+            {
+                if (result.IsLockedOut)
+                {
+                    ModelState.AddModelError("Login", "You are locked out");
+                }
+                else
+                {
+                    ModelState.AddModelError("Login", "Failed to login");
+                }
+            }
+            return Page();
+        }
+    }
+}
